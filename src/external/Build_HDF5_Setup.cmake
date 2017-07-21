1b3b17b48bca9c407fc3f97ffe3c6039fdd505aa
andrew-platt <andrew.platt@envision-energy.com>
2017-07-21 17:53:56 -0600

    Slight improvements.  Can't compile with found module on mac, but works fine on linux.  Static only tested at moment

--- src/external/Build_HDF5_Setup.cmake
+++ src/external/Build_HDF5_Setup.cmake
@@ -22,81 +22,136 @@
            message(STATUS "The compiler ${CMAKE_CXX_COMPILER} has no C++11 support. Please use a different C++ compiler.")
    endif()
   
-#Not sure if this logic works
-   if (HDF5_COMPRESSION)
-      set(HDF5_DEPENDS "zlib")
-      find_package(ZLIB)
-      add_library(zlib UNKNOWN IMPORTED)
-      set_target_properties(zlib PROPERTIES IMPORTED_LOCATION ${ZLIB_LIBRARY})
-   else()
-      set(HDF5_DEPENDS "")
-   endif()
-
 
 
    set(HDF5_CMAKE_ARGS "")
    list(APPEND HDF5_CMAKE_ARGS
-      -DCMAKE_INSTALL_PREFIX=${CMAKE_CURRENT_BINARY_DIR}/3rd_party
-      -DCMAKE_Fortran_MODULE_DIRECTORY=${HDF5_CMAKE_Fortran_MODULE_DIRECTORY}
-      -DHDF5_BUILD_FORTRAN=ON
-      -DHDF5_BUILD_CPP_LIB=ON
-      -DBUILD_SHARED_LIBS=${BUILD_SHARED_LIBS}
-      -DBUILD_TESTING=OFF
-      -DHDF5_BUILD_EXAMPLES=OFF
-      -DHDF5_BUILD_TOOLS=OFF
-      -DHDF5_BUILD_HL_LIB=OFF
-      -DHDF5_NO_PACKAGES=OFF
-      -DHDF5_PACKAGE_EXTLIBS=OFF
-      -DALLOW_UNSUPPORTED=ON
-      -DCMAKE_BUILD_TYPE=Release
-      -DHDF5_INSTALL_LIB_DIR=${3rd_party_LIB_DIR}
-      -DHDF5_INSTALL_BIN_DIR=${3rd_party_BIN_DIR})
-      #-DHDF5_ENABLE_Z_LIB_SUPPORT  ON
-      #-DHDF5_ALLOW_EXTERNAL_SUPPORT  TGZ
-      #-DTGZPATH  ${CMAKE_SOURCE_DIR}/external
-      #-DZLIB_TGZ_NAME  ZLib.tar.gz
-      #-DZLIB_PACKAGE_NAME  zlib
-      #-DZLIB_EXTERNALLY_CONFIGURED  ON
-      ##-DZLIB_INSTALL_BIN_DIR  "${CMAKE_CURRENT_BINARY_DIR}/bin"
-      ##-DZLIB_INSTALL_LIB_DIR  "${CMAKE_CURRENT_BINARY_DIR}/lib"
-      #-DHDF5_ENABLE_SZIP_SUPPORT  OFF
-      #-DHDF5_ENABLE_SZIP_ENCODING  OFF
+      -DCMAKE_INSTALL_PREFIX:STRING=${3rd_party_BUILD_DIR}
+      -DCMAKE_Fortran_MODULE_DIRECTORY:STRING=${HDF5_CMAKE_Fortran_MODULE_DIRECTORY}
+      -DCMAKE_LIBRARY_OUTPUT_DIRECTORY:STRING=${3rd_party_LIB_DIR}
+      -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
+      -DHDF5_BUILD_FORTRAN:BOOL=ON
+      -DHDF5_BUILD_CPP_LIB:BOOL=ON
+      -DBUILD_SHARED_LIBS:STRING=${BUILD_SHARED_LIBS}
+      -DHDF5_EXPORTED_TARGETS:STRING=hdf5-targets
+      -DBUILD_TESTING:BOOL=OFF
+      -DHDF5_BUILD_EXAMPLES:BOOL=OFF
+      -DHDF5_BUILD_TOOLS:BOOL=OFF
+      -DHDF5_BUILD_HL_LIB:BOOL=OFF
+      -DHDF5_NO_PACKAGES:BOOL=OFF
+      -DHDF5_PACKAGE_EXTLIBS:BOOL=OFF
+      -DALLOW_UNSUPPORTED:BOOL=ON)
+#      -DHDF5_LIBRARIES_TO_EXPORT="hdf5;hdf5_fortran;hdf5_cpp;hdf5_f90cstub")
+
+
+
+
+   if(HDF5_COMPRESSION)
+      list(APPEND HDF5_CMAKE_ARGS
+         -DHDF5_ENABLE_Z_LIB_SUPPORT=ON)
+
+      if( BUILD_ZLIB )
+         set(HDF5_DEPENDS "localZLIB")
+         set(localZLIB_args "")
+         list(APPEND localZLIB_args
+            -DZLIB_EXTERNALLY_CONFIGURED:BOOL=ON
+            -DCMAKE_INSTALL_PREFIX=${3rd_party_BUILD_DIR}
+            -DBUILD_SHARED_LIBS:BOOL=${BUILD_SHARED_LIBS}
+            -DZLIB_PACKAGE_EXT:STRING=${HDF_PACKAGE_EXT}
+            -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
+            -DCMAKE_ANSI_CFLAGS:STRING=${CMAKE_ANSI_CFLAGS})
+
+         ExternalProject_Add(localZLIB
+            PREFIX 3rd_party
+            URL ${CMAKE_SOURCE_DIR}/external/ZLib.tar.gz
+            CMAKE_ARGS ${localZLIB_args})
+  
+         ExternalProject_Get_Property (localZLIB BINARY_DIR SOURCE_DIR INSTALL_DIR)
+
+         add_library(ZLIB UNKNOWN IMPORTED)
+
+         if (WIN32)
+            set (localZLIB_LIB_NAME "zlib")
+         else ()
+            set (localZLIB_LIB_NAME "z")
+         endif ()
+         set (localZLIB_INCLUDE_DIR     ${INSTALL_DIR})
+         set (localZLIB_INCLUDE_DIR_GEN ${SOURCE_DIR})
+         set (localZLIB_LIBRARY ${3rd_party_LIB_DIR}/${CMAKE_STATIC_LIBRARY_PREFIX}${localZLIB_LIB_NAME}${CMAKE_STATIC_LIBRARY_SUFFIX})
+         set (ZLIB_FOUND 1)
+         set (localZLIB_INCLUDE_DIRS ${localZLIB_INCLUDE_DIR} ${localZLIB_INCLUDE_DIR_GEN})
+         set (ZLIB_LIBRARIES ${localZLIB_LIB_NAME})
+
+            # Now set some more info for the HDF5 compile
+         list(APPEND HDF5_CMAKE_ARGS
+            -DZLIB_USE_EXTERNAL:BOOL=ON
+            -DH5_ZLIB_HEADER:STRING=zlib.h
+            -DZLIB_LIBRARY:FILEPATH=${localZLIB_LIBRARY}
+            -DZLIB_INCLUDE_DIR:PATH=${localZLIB_INCLUDE_DIRS})
+      else( BUILD_ZLIB )
+         # Force it to use the system one if it can find it (which it should have before when the BUILD_ZLIB flag was set)
+         list(APPEND HDF5_CMAKE_ARGS
+            -DHDF5_ALLOW_EXTERNAL_SUPPORT:BOOL=OFF
+            -DZLIB_USE_EXTERNAL:BOOL=OFF)
+      endif()
+
+
+   else()
+      list(APPEND HDF5_CMAKE_ARGS
+      -DHDF5_ENABLE_Z_LIB_SUPPORT:BOOL=OFF
+      -DHDF5_ENABLE_SZIP_SUPPORT:BOOL=OFF
+      -DHDF5_ENABLE_SZIP_ENCODING:BOOL=OFF)
+   endif()
+
+
 
 
 
 
       # Add the HDF5 project
-   ExternalProject_Add(HDF5
+   ExternalProject_Add(localHDF5
          PREFIX 3rd_party
          DEPENDS ${HDF5_DEPENDS}
          CMAKE_ARGS ${HDF5_CMAKE_ARGS}
+         INSTALL_DIR ${3rd_party_LIB_DIR}
+#???         install (EXPORT hdf5-targets)
+#         SOURCE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/external/hdf5-1.10.1")
          SOURCE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/external/hdf5-1.8.19")
 
-#   add_library(HDF5 IMPORTED)
+   externalproject_get_property (localHDF5 BINARY_DIR SOURCE_DIR INSTALL_DIR)
+   add_library(HDF5 UNKNOWN IMPORTED GLOBAL)
+
+
+#FIXME: this should be possible to do, but I'm not sure how just yet.  How do I get the targets?
+#   set (HDF5_INCLUDE_DIR_GEN ${BINARY_DIR})
+#   set (HDF5_INCLUDE_DIR ${SOURCE_DIR})
+#   set (HDF5_INCLUDE_DIRS ${HDF5_INCLUDE_DIR} ${HDF5_INCLUDE_DIR_GEN}) 
+
+
+      # Can't find a way to automate this.  Not sure how to get the hdf5-targets exported from the HDF5 build.
    if (${LIB_TYPE} MATCHES "SHARED")
       set(HDF5_LIBRARIES
-         ${CMAKE_SHARED_LIBRARY_PREFIX}hdf5_fortran${CMAKE_SHARED_LIBRARY_SUFFIX}
-         ${CMAKE_SHARED_LIBRARY_PREFIX}hdf5_f90cstub${CMAKE_SHARED_LIBRARY_SUFFIX}
-         ${CMAKE_SHARED_LIBRARY_PREFIX}hdf5_cpp${CMAKE_SHARED_LIBRARY_SUFFIX}
-         ${CMAKE_SHARED_LIBRARY_PREFIX}hdf5${CMAKE_SHARED_LIBRARY_SUFFIX}      )
+         ${CMAKE_SHARED_LIBRARY_PREFIX}hdf5_fortran-shared${CMAKE_SHARED_LIBRARY_SUFFIX}
+         ${CMAKE_SHARED_LIBRARY_PREFIX}hdf5_f90cstub-shared${CMAKE_SHARED_LIBRARY_SUFFIX}
+         ${CMAKE_SHARED_LIBRARY_PREFIX}hdf5_cpp-shared${CMAKE_SHARED_LIBRARY_SUFFIX}
+         ${CMAKE_SHARED_LIBRARY_PREFIX}hdf5-shared${CMAKE_SHARED_LIBRARY_SUFFIX}      )
    elseif (${LIB_TYPE} MATCHES "STATIC")
       set(HDF5_LIBRARIES
-         ${CMAKE_STATIC_LIBRARY_PREFIX}hdf5_fortran${CMAKE_STATIC_LIBRARY_SUFFIX}
-         ${CMAKE_STATIC_LIBRARY_PREFIX}hdf5_f90cstub${CMAKE_STATIC_LIBRARY_SUFFIX}
-         ${CMAKE_STATIC_LIBRARY_PREFIX}hdf5_cpp${CMAKE_STATIC_LIBRARY_SUFFIX}
-         ${CMAKE_STATIC_LIBRARY_PREFIX}hdf5${CMAKE_STATIC_LIBRARY_SUFFIX}      )
+         ${CMAKE_STATIC_LIBRARY_PREFIX}hdf5_fortran-static${CMAKE_STATIC_LIBRARY_SUFFIX}
+         ${CMAKE_STATIC_LIBRARY_PREFIX}hdf5_f90cstub-static${CMAKE_STATIC_LIBRARY_SUFFIX}
+         ${CMAKE_STATIC_LIBRARY_PREFIX}hdf5_cpp-static${CMAKE_STATIC_LIBRARY_SUFFIX}
+         ${CMAKE_STATIC_LIBRARY_PREFIX}hdf5-static${CMAKE_STATIC_LIBRARY_SUFFIX}      )
    endif()
+
+
 
 
       ##########################################
       # set some names for compilation / linking
-#   set (HDF5_LIBRARIES  "")
-#   if (MSVC)
-#      list(APPEND HDF5_LIBRARIES  libhdf5_fortran libhdf5_f90cstub libhdf5_cpp libhdf5)   # MS visual studo does not append the -static or -shared to the lib
-#   else()
-##      list(APPEND HDF5_LIBRARIES  hdf5_fortran-${SEARCH_TYPE} hdf5_f90cstub-${SEARCH_TYPE} hdf5_cpp-${SEARCH_TYPE} hdf5-${SEARCH_TYPE})
-#      list(APPEND HDF5_LIBRARIES  hdf5_fortran hdf5_f90cstub hdf5_cpp hdf5)
-#   endif()
+   if (HDF5_COMPRESSION)
+      set(HDF5_LIBRARIES ${HDF5_LIBRARIES} ${ZLIB_LIBRARIES})
+   endif()
+
 
       # Linux with gfortran needs the libdl library
    get_filename_component(FCNAME ${CMAKE_Fortran_COMPILER} NAME)
